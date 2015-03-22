675f2886c0872401cae1548ac5bbf79519aa92cf
adriano <contato@adrianoweb.com.br>
2015-03-22 10:07:09 -0300

    Make it work with type number fields #1

--- mountable.js
+++ mountable.js
@@ -7,236 +7,235 @@
 $.fn.mounTable = function (content, options)
 {
 
-	var opt =
-	{
-		debug: (options && options.noDebug) ? false : true,
-		fn:
-		{
-			element: 		"",
-			model:
-			{
-				class: 		(options && options.model) ? options.model : ".mountable-model",
-				content: 	"",
-				length: 	Object.keys($(this).find((options && options.model) ? options.model : ".mountable-model")).length,
-				newLine: 	(options && options.addLine && options.addLine.button) ? options.addLine.button : ".mountable-new-line",
-				origin: 	"",
-				saved: 		"",
-			},
-			table: this
-		}
-	};
+    var opt =
+            {
+                debug: (options && options.noDebug) ? false : true,
+                fn:
+                        {
+                            element: "",
+                            model:
+                                    {
+                                        class: (options && options.model) ? options.model : ".mountable-model",
+                                        content: "",
+                                        length: Object.keys($(this).find((options && options.model) ? options.model : ".mountable-model")).length,
+                                        newLine: (options && options.addLine && options.addLine.button) ? options.addLine.button : ".mountable-new-line",
+                                        origin: "",
+                                        saved: "",
+                                    },
+                            table: this
+                        }
+            };
 
-	var fn =
-	{
+    var fn =
+            {
+                init: function ()
+                {
 
-		init: function ()
-		{
+                    if (content && content.length)
+                    {
 
-			if (content && content.length)
-			{
+                        if (!$(opt.fn.table).length)
+                        {
 
-				if (!$(opt.fn.table).length)
-				{
+                            if (opt.debug)
+                            {
+                                console.log('MounTable: Table ' + opt.fn.table.selector + ' not found!');
+                            }
 
-					if (opt.debug)
-					{
-						console.log('MounTable: Table ' + opt.fn.table.selector + ' not found!');
-					}
+                        }
+                        else
+                        {
 
-				}
-				else
-				{
+                            $.each(content, function (i, line)
+                            {
 
-					$.each(content, function (i, line)
-					{
+                                if (line)
+                                {
 
-						if (line)
-						{
+                                    $.each(line, function (j, input)
+                                    {
 
-							$.each(line, function (j, input)
-							{
+                                        if ($.type(input) === 'string' || $.type(input) === 'number')
+                                        {
 
-								if ($.type(input) === 'string' || $.type(input) === 'number')
-								{
+                                            opt.fn.element = $(opt.fn.table).find(opt.fn.model.class).find('input[name="' + j + '[]"], input[name="' + j + '"], select[name="' + j + '[]"], select[name="' + j + '"]');
 
-									opt.fn.element = $(opt.fn.table).find(opt.fn.model.class).find('input[name="' + j + '[]"], input[name="' + j + '"], select[name="' + j + '[]"], select[name="' + j + '"]');
+                                            if (opt.fn.element.prop('tagName') == "INPUT")
+                                            {
 
-									if (opt.fn.element.prop('tagName') == "INPUT")
-									{
+                                                if (opt.fn.element.attr('type') == 'text' || opt.fn.element.attr('type') == 'number')
+                                                {
+                                                    opt.fn.element.attr('value', input);
+                                                }
+                                                else if (opt.fn.element.attr('type') == 'checkbox')
+                                                {
 
-										if (opt.fn.element.attr('type') == 'text')
-										{
-											opt.fn.element.attr('value', input);
-										}
-										else if (opt.fn.element.attr('type') == 'checkbox')
-										{
+                                                    if (opt.fn.element.length > 1)
+                                                    {
 
-											if (opt.fn.element.length > 1)
-											{
+                                                        $.each(opt.fn.element, function (k, checkbox)
+                                                        {
 
-												$.each(opt.fn.element, function (k, checkbox)
-												{
+                                                            if (checkbox.value == input)
+                                                            {
+                                                                checkbox.setAttribute('checked', 'checked');
+                                                            }
+                                                            else
+                                                            {
+                                                                checkbox.removeAttribute('checked');
+                                                            }
 
-													if (checkbox.value == input)
-													{
-														checkbox.setAttribute('checked', 'checked');
-													}
-													else
-													{
-														checkbox.removeAttribute('checked');
-													}
+                                                        });
 
-												});
+                                                    }
 
-											}
+                                                }
 
-										}
+                                            }
+                                            else if (opt.fn.element.prop('tagName') == "SELECT")
+                                            {
 
-									}
-									else if (opt.fn.element.prop('tagName') == "SELECT")
-									{
+                                                opt.fn.element.val(input);
+                                                opt.fn.element.children('option').attr('selected', false);
+                                                opt.fn.element.children('option[value="' + input + '"]').attr('selected', 'selected');
 
-										opt.fn.element.val(input);
-										opt.fn.element.children('option').attr('selected', false);
-										opt.fn.element.children('option[value="' + input + '"]').attr('selected', 'selected');
+                                            }
 
-									}
+                                        }
+                                        else if ($.type(input) === 'array')
+                                        {
 
-								}
-								else if ($.type(input) === 'array')
-								{
+                                            opt.fn.element = $(opt.fn.table).find(opt.fn.model.class).find('select[name="' + j + '[]"], select[name="' + j + '"]');
 
-									opt.fn.element = $(opt.fn.table).find(opt.fn.model.class).find('select[name="' + j + '[]"], select[name="' + j + '"]');
+                                            if (opt.fn.element.prop('tagName') == "SELECT")
+                                            {
 
-									if (opt.fn.element.prop('tagName') == "SELECT")
-									{
+                                                selectOpts = false;
 
-										selectOpts = false;
+                                                $.each(input, function (l, value)
+                                                {
+                                                    selectOpts += '<option value="' + value + '">' + value + '</option>';
+                                                });
 
-										$.each(input, function(l, value)
-										{
-											selectOpts += '<option value="' + value + '">' + value + '</option>';
-										});
+                                                opt.fn.element.html(selectOpts);
+                                                opt.fn.element.attr('data-mountable-filled-up-selectbox', 'yes');
 
-										opt.fn.element.html(selectOpts);
-										opt.fn.element.attr('data-mountable-filled-up-selectbox', 'yes');
+                                            }
+                                            ;
 
-									};
+                                        }
 
-								}
+                                    });
 
-							});
+                                    opt.fn.model.dom = $(opt.fn.table).find(opt.fn.model.class);
+                                    opt.fn.model.content = $(opt.fn.table).find(opt.fn.model.class).html();
 
-							opt.fn.model.dom = $(opt.fn.table).find(opt.fn.model.class);
-							opt.fn.model.content = $(opt.fn.table).find(opt.fn.model.class).html();
+                                    $(opt.fn.table).find(opt.fn.model.class).parent().append('<tr>' + opt.fn.model.content + '</tr>');
 
-							$(opt.fn.table).find(opt.fn.model.class).parent().append('<tr>' + opt.fn.model.content + '</tr>');
+                                    $(opt.fn.table).find(opt.fn.model.class + ' input').each(function ()
+                                    {
 
-							$(opt.fn.table).find(opt.fn.model.class + ' input').each(function()
-							{
+                                        if ($(this).prop('type') == "text" || $(this).prop('type') == "number")
+                                        {
+                                            $(this).attr('value', "");
+                                        }
+                                        else if ($(this).prop('type') == "checkbox")
+                                        {
 
-								if ($(this).prop('type') == "text")
-								{
-									$(this).attr('value', "");
-								}
-								else if ($(this).prop('type') == "checkbox")
-								{
+                                            $(this).each(function ()
+                                            {
+                                                $(this).attr('checked', false);
+                                            });
 
-									$(this).each(function()
-									{
-										$(this).attr('checked', false);
-									});
+                                        }
 
-								}
+                                    });
 
-							});
+                                    $(opt.fn.table).find(opt.fn.model.class + ' select').each(function ()
+                                    {
 
-							$(opt.fn.table).find(opt.fn.model.class + ' select').each(function()
-							{
+                                        $(this).find('option').each(function ()
+                                        {
+                                            $(this).removeAttr('selected');
+                                        });
 
-								$(this).find('option').each(function()
-								{
-									$(this).removeAttr('selected');
-								});
+                                        $(this).attr('value', "");
 
-								$(this).attr('value', "");
+                                        if ($(this).attr('data-mountable-filled-up-selectbox') == 'yes')
+                                        {
+                                            $(this).html(' ');
+                                        }
 
-								if ($(this).attr('data-mountable-filled-up-selectbox') == 'yes')
-								{
-									$(this).html(' ');
-								}
+                                    });
 
-							});
+                                }
 
-						}
+                            });
 
-					});
+                        }
 
-				}
+                        opt.fn.model.saved = '<tr>' + $(opt.fn.table).find(opt.fn.model.class).html() + '</tr>';
+                        opt.fn.model.origin = $(opt.fn.table).find(opt.fn.model.class).parent();
 
-				opt.fn.model.saved = '<tr>' + $(opt.fn.table).find(opt.fn.model.class).html() + '</tr>';
-				opt.fn.model.origin = $(opt.fn.table).find(opt.fn.model.class).parent();
+                        if (opt.debug)
+                        {
+                            console.log('MounTable: content successfully mounted on ' + opt.fn.table.selector);
+                        }
 
-				if (opt.debug)
-				{
-					console.log('MounTable: content successfully mounted on ' + opt.fn.table.selector);
-				}
+                        $(opt.fn.model.newLine).off('click').on('click', function ()
+                        {
 
-				$(opt.fn.model.newLine).off('click').on('click', function()
-				{
+                            if (options.addLine.onClick && $.type(options.addLine.onClick) === "function")
+                            {
 
-					if (options.addLine.onClick && $.type(options.addLine.onClick) === "function")
-					{
+                                if (options.addLine.onClick($(opt.fn.table).find(opt.fn.model.class)) === true)
+                                {
 
-						if (options.addLine.onClick($(opt.fn.table).find(opt.fn.model.class)) === true)
-						{
+                                    opt.fn.model.origin.append(opt.fn.model.saved);
+                                    fn.deleteLine();
 
-							opt.fn.model.origin.append(opt.fn.model.saved);
-							fn.deleteLine();
+                                }
 
-						}
+                            }
 
-					}
+                        });
 
-				});
+                        fn.deleteLine();
+                        $(opt.fn.table).find(opt.fn.model.class).remove();
 
-				fn.deleteLine();
-				$(opt.fn.table).find(opt.fn.model.class).remove();
+                    }
 
-			}
+                },
+                deleteLine: function ()
+                {
+                    if (options && options.deleteLine && options.deleteLine.button)
+                    {
 
-		},
+                        $(options.deleteLine.button).off('click').on('click', function ()
+                        {
 
-		deleteLine: function ()
-		{
-			if (options && options.deleteLine && options.deleteLine.button)
-			{
+                            if (options.deleteLine.onClick && $.type(options.deleteLine.onClick) === "function")
+                            {
 
-				$(options.deleteLine.button).off('click').on('click', function()
-				{
+                                if (options.deleteLine.onClick($(this).parent().parent()) === true)
+                                {
+                                    $(this).parent().parent().remove();
+                                }
 
-					if (options.deleteLine.onClick && $.type(options.deleteLine.onClick) === "function")
-					{
+                            }
+                            else
+                            {
+                                $(this).parent().parent().remove();
+                            }
 
-						if (options.deleteLine.onClick($(this).parent().parent()) === true)
-						{
-							$(this).parent().parent().remove();
-						}
+                        });
 
-					}
-					else
-					{
-						$(this).parent().parent().remove();
-					}
+                    }
 
-				});
+                }
 
-			}
+            };
 
-		}
-
-	};
-
-	fn.init();
+    fn.init();
 
 };
